958d79528e0307bfcc97d904245dba36ec3f7178
javiergcas <javiergcas@gmail.com>
2022-07-14 16:11:28 -0400

    Accessory functions used across different notebooks

---
+++ Notebooks/utils/basics.py
@@ -0,0 +1,4 @@
+PRJ_DIR = '/data/SFIMJGC_HCP7T/manifold_learning_fmri'
+
+task_cmap      = {'Rest': 'gray', 'Memory': 'blue', 'Video': '#F4D03F', 'Math': 'green', 'Inbetween': 'black'}
+task_cmap_caps = {'REST': 'gray', 'BACK': 'blue',   'VIDE': '#F4D03F',  'MATH': 'green', 'XXXX': 'black'}
