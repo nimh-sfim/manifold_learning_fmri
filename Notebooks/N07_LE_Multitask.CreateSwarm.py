6c407f1f786ae49faa4f3849779d4d7444dada74
javiergcas <javiergcas@gmail.com>
2022-10-24 11:48:43 -0400

    Updated to run all necessary scenarios

--- Notebooks/N07_LE_Multitask.CreateSwarm.py
+++ Notebooks/N07_LE_Multitask.CreateSwarm.py
@@ -48,12 +48,18 @@ print('++ INFO: Distance Metrics: %s' % str(le_dist_metrics))
 print('++ INFO: Knns:             %s' % str(le_knns))
 print('++ INFO: Ms:               %s' % str(le_ms))
 
+# ***
+# # 1. Scan-Level : 
+#
+# ## 1.1. Original Data
+
 # The next cell will create the output folders if they do not exist already
 
 # Create Output Folders if they do not exists
 for subject in PNAS2015_subject_list:
-    path = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE')
+    path = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Original')
     if not osp.exists(path):
+        print('++ INFO: Created folder %s' % path)
         os.makedirs(path)
 
 # The next cell will create folders for the swarm log files and for the actual swarm script. Those folders are created using the username as part of their name. That way it is easier for different users to work together on the project.
@@ -67,13 +73,15 @@ print('++ INFO: user working now --> %s' % username)
 swarm_folder   = osp.join(PRJ_DIR,'SwarmFiles.{username}'.format(username=username))
 logs_folder    = osp.join(PRJ_DIR,'Logs.{username}'.format(username=username))  
 
-swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask.SWARM.sh')
-logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask.logs')
+swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_Scans_Original.SWARM.sh')
+logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_Scans_Original.logs')
 
 if not osp.exists(swarm_folder):
     os.makedirs(swarm_folder)
 if not osp.exists(logdir_path):
     os.makedirs(logdir_path)
+print('++ INFO: Swarm file : %s' % swarm_path)
+print('++ INFO: Logs folder: %s' % logdir_path)
 # -
 
 # Create swarm script. This script will have one line per matrix to be generated.
@@ -86,38 +94,40 @@ swarm_file.write('#Create Time: %s' % datetime.now().strftime("%d/%m/%Y %H:%M:%S
 swarm_file.write('\n')
 
 # Insert comment line with SWARM command
-swarm_file.write('#swarm -f {swarm_path} -b 20 -g 4 -t 4 --time 00:05:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path,logdir_path=logdir_path))
+swarm_file.write('#swarm -f {swarm_path} -b 15 -g 4 -t 4 --time 00:05:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path,logdir_path=logdir_path))
 swarm_file.write('\n')
 
 for subject in PNAS2015_subject_list:
     for dist in le_dist_metrics:
         for knn in le_knns:
             for m in le_ms:
-                path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.R.pkl'.format(subject=subject,wls=str(int(wls)).zfill(3), wss=str(wss)))
-                path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.pkl'.format(subject=subject,
+                for scenario in ['asis','zscored']:
+                    path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'Original',     '{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.Z.{scenario}.pkl'.format(subject=subject,scenario=scenario,wls=str(int(wls)).zfill(3), wss=str(wss)))
+                    path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Original','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.{scenario}.pkl'.format(subject=subject,scenario=scenario,
                                                                                                                                                    wls=str(int(wls)).zfill(3), 
                                                                                                                                                    wss=str(wss),
                                                                                                                                                    dist=dist,
                                                                                                                                                    knn=str(knn).zfill(4),
                                                                                                                                                    m=str(m).zfill(4)))
-                swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
+                    swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
                                                                                                                                     path_out=path_out,
                                                                                                                                     dist=dist,
                                                                                                                                     knn=knn,
                                                                                                                                     m=m, 
                                                                                                                                     scripts_dir=osp.join(PRJ_DIR,'Notebooks')))
-                swarm_file.write('\n')
+                    swarm_file.write('\n')
 swarm_file.close()
 
 # + [markdown] tags=[]
 # ***
-# # Run on Connection Randomization Model
+# ## 1.2. Connection Randomization
 # -
 
 # Create Output Folders if they do not exists
 for subject in PNAS2015_subject_list:
     path = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_ConnRand')
     if not osp.exists(path):
+        print('++ INFO: Created folder %s' % path)
         os.makedirs(path)
 
 # The next cell will create folders for the swarm log files and for the actual swarm script. Those folders are created using the username as part of their name. That way it is easier for different users to work together on the project.
@@ -131,13 +141,15 @@ print('++ INFO: user working now --> %s' % username)
 swarm_folder   = osp.join(PRJ_DIR,'SwarmFiles.{username}'.format(username=username))
 logs_folder    = osp.join(PRJ_DIR,'Logs.{username}'.format(username=username))  
 
-swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_ConnRand.SWARM.sh')
-logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_ConnRand.logs')
+swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_Scans_ConnRand.SWARM.sh')
+logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_Scans_ConnRand.logs')
 
 if not osp.exists(swarm_folder):
     os.makedirs(swarm_folder)
 if not osp.exists(logdir_path):
     os.makedirs(logdir_path)
+print('++ INFO: Swarm file : %s' % swarm_path)
+print('++ INFO: Logs folder: %s' % logdir_path)
 # -
 
 # Create swarm script. This script will have one line per matrix to be generated.
@@ -150,27 +162,28 @@ swarm_file.write('#Create Time: %s' % datetime.now().strftime("%d/%m/%Y %H:%M:%S
 swarm_file.write('\n')
 
 # Insert comment line with SWARM command
-swarm_file.write('#swarm -f {swarm_path} -b 20 -g 4 -t 4 --time 00:05:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path,logdir_path=logdir_path))
+swarm_file.write('#swarm -f {swarm_path} -b 15 -g 4 -t 4 --time 00:05:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path,logdir_path=logdir_path))
 swarm_file.write('\n')
 
 for subject in PNAS2015_subject_list:
     for dist in le_dist_metrics:
         for knn in le_knns:
             for m in le_ms:
-                path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'Null_ConnRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.R.pkl'.format(subject=subject,wls=str(int(wls)).zfill(3), wss=str(wss)))
-                path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_ConnRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.pkl'.format(subject=subject,
+                for scenario in ['asis','zscored']:
+                    path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'Null_ConnRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.Z.{scenario}.pkl'.format(subject=subject,scenario=scenario,wls=str(int(wls)).zfill(3), wss=str(wss)))
+                    path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_ConnRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.{scenario}.pkl'.format(subject=subject,scenario=scenario,
                                                                                                                                                    wls=str(int(wls)).zfill(3), 
                                                                                                                                                    wss=str(wss),
                                                                                                                                                    dist=dist,
                                                                                                                                                    knn=str(knn).zfill(4),
                                                                                                                                                    m=str(m).zfill(4)))
-                swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
+                    swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
                                                                                                                                     path_out=path_out,
                                                                                                                                     dist=dist,
                                                                                                                                     knn=knn,
                                                                                                                                     m=m, 
                                                                                                                                     scripts_dir=osp.join(PRJ_DIR,'Notebooks')))
-                swarm_file.write('\n')
+                    swarm_file.write('\n')
 swarm_file.close()
 
 # + [markdown] tags=[]
@@ -182,6 +195,7 @@ swarm_file.close()
 for subject in PNAS2015_subject_list:
     path = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_PhaseRand')
     if not osp.exists(path):
+        print('++ INF: Created folder %s' % path)
         os.makedirs(path)
 
 # The next cell will create folders for the swarm log files and for the actual swarm script. Those folders are created using the username as part of their name. That way it is easier for different users to work together on the project.
@@ -195,13 +209,15 @@ print('++ INFO: user working now --> %s' % username)
 swarm_folder   = osp.join(PRJ_DIR,'SwarmFiles.{username}'.format(username=username))
 logs_folder    = osp.join(PRJ_DIR,'Logs.{username}'.format(username=username))  
 
-swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_PhaseRand.SWARM.sh')
-logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_PhaseRand.logs')
+swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_Scans_PhaseRand.SWARM.sh')
+logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_Scans_PhaseRand.logs')
 
 if not osp.exists(swarm_folder):
     os.makedirs(swarm_folder)
 if not osp.exists(logdir_path):
     os.makedirs(logdir_path)
+print('++ INFO: Swarm file : %s' % swarm_path)
+print('++ INFO: Logs folder: %s' % logdir_path)
 # -
 
 # Create swarm script. This script will have one line per matrix to be generated.
@@ -221,21 +237,91 @@ for subject in PNAS2015_subject_list:
     for dist in le_dist_metrics:
         for knn in le_knns:
             for m in le_ms:
-                path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'Null_PhaseRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.R.pkl'.format(subject=subject,wls=str(int(wls)).zfill(3), wss=str(wss)))
-                path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_PhaseRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.pkl'.format(subject=subject,
+                for scenario in ['asis','zscored']:
+                    path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'Null_PhaseRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.Z.{scenario}.pkl'.format(subject=subject,scenario=scenario,wls=str(int(wls)).zfill(3), wss=str(wss)))
+                    path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015',subject,'LE','Null_PhaseRand','{subject}_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.{scenario}.pkl'.format(subject=subject,scenario=scenario,
                                                                                                                                                    wls=str(int(wls)).zfill(3), 
                                                                                                                                                    wss=str(wss),
                                                                                                                                                    dist=dist,
                                                                                                                                                    knn=str(knn).zfill(4),
                                                                                                                                                    m=str(m).zfill(4)))
-                swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
+                    swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
                                                                                                                                     path_out=path_out,
                                                                                                                                     dist=dist,
                                                                                                                                     knn=knn,
                                                                                                                                     m=m, 
                                                                                                                                     scripts_dir=osp.join(PRJ_DIR,'Notebooks')))
-                swarm_file.write('\n')
+                    swarm_file.write('\n')
 swarm_file.close()
 # -
+# ***
+#
+# # Group Level Analyses
+
+# Create Output Folders if they do not exists
+for input_data in ['Original','Null_ConnRand','Null_PhaseRand']:
+    path = osp.join(PRJ_DIR,'Data_Interim','PNAS2015','ALL','LE',input_data)
+    if not osp.exists(path):
+        print('++ INF: Created folder %s' % path)
+        os.makedirs(path)
+
+
+# +
+#user specific folders
+#=====================
+username = getpass.getuser()
+print('++ INFO: user working now --> %s' % username)
+
+swarm_folder   = osp.join(PRJ_DIR,'SwarmFiles.{username}'.format(username=username))
+logs_folder    = osp.join(PRJ_DIR,'Logs.{username}'.format(username=username))  
+
+swarm_path     = osp.join(swarm_folder,'N07_LE_Multitask_ALL.SWARM.sh')
+logdir_path    = osp.join(logs_folder, 'N07_LE_Multitask_ALL.logs')
+
+if not osp.exists(swarm_folder):
+    os.makedirs(swarm_folder)
+if not osp.exists(logdir_path):
+    os.makedirs(logdir_path)
+print('++ INFO: Swarm file : %s' % swarm_path)
+print('++ INFO: Logs folder: %s' % logdir_path)
+
+# +
+# Open the file
+swarm_file = open(swarm_path, "w")
+# Log the date and time when the SWARM file is created
+swarm_file.write('#Create Time: %s' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+swarm_file.write('\n')
+
+# Insert comment line with SWARM command
+swarm_file.write('#swarm -f {swarm_path} -b 3 -g 24 -t 24 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path,logdir_path=logdir_path))
+swarm_file.write('\n')
+missing_files = 0
+needed_files = 0
+for scenario in ['asis','zscored']:
+    for dist in le_dist_metrics:
+        for knn in le_knns:
+            for m in le_ms:
+                for input_data in ['Original','Null_ConnRand','Null_PhaseRand']:
+                    needed_files += 1
+                    path_tvfc = osp.join(PRJ_DIR,'Data_Interim','PNAS2015','ALL',input_data,     'ALL_Craddock_0200.WL{wls}s.WS{wss}s.tvFC.Z.{scenario}.pkl'.format(scenario=scenario,wls=str(int(wls)).zfill(3), wss=str(wss)))
+                    path_out  = osp.join(PRJ_DIR,'Data_Interim','PNAS2015','ALL','LE',input_data,'ALL_Craddock_0200.WL{wls}s.WS{wss}s.LE_{dist}_k{knn}_m{m}.{scenario}.pkl'.format(scenario=scenario,
+                                                                                                                                                   wls=str(int(wls)).zfill(3), 
+                                                                                                                                                   wss=str(wss),
+                                                                                                                                                   dist=dist,
+                                                                                                                                                   knn=str(knn).zfill(4),
+                                                                                                                                                   m=str(m).zfill(4)))
+                    if not osp.exists(path_out):
+                        missing_files +=1
+                        swarm_file.write('export path_tvfc={path_tvfc} dist={dist} knn={knn} m={m} path_out={path_out}; sh {scripts_dir}/N07_LE.sh'.format(path_tvfc=path_tvfc, 
+                                                                                                                                    path_out=path_out,
+                                                                                                                                    dist=dist,
+                                                                                                                                    knn=knn,
+                                                                                                                                    m=m, 
+                                                                                                                                    scripts_dir=osp.join(PRJ_DIR,'Notebooks')))
+                        swarm_file.write('\n')
+swarm_file.close()
+print('++ INFO: Missing / Needed Files [%d/%d]' % (missing_files, needed_files))
+# -
+
 
 
