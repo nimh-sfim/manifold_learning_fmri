134c67a2f939d1f430cac7184c0525a211d6e45b
javiergcas <javiergcas@gmail.com>
2022-11-22 09:59:10 -0500

    Bug Corrected: was using embs with XXXX to compute SI

--- Notebooks/N10_SI.py
+++ Notebooks/N10_SI.py
@@ -16,45 +16,50 @@ def run(args):
     # ==============
     print('++ INFO: Loading Embedding and labels: [%s]' % input_path)
     emb = pd.read_pickle(input_path)
-    print(' +       emb.shape=%s' % str(emb.shape))
     n_wins,m = emb.shape
-    print(' + Original Embedding DataFrame Size = %s' % str(emb.shape))
-    print(' + Number of Dimensions = %d' % m)
+    print(' +       Original Embedding DataFrame Size = %s' % str(emb.shape))
+    print(' +       Number of Dimensions = %d' % m)
     # Temporary Fix for UMAP
-    emb.index.name = 'Window Name'
-    emb.to_pickle(input_path)
+    # emb.index.name = 'Window Name'
+    # emb.to_pickle(input_path)
     # End of Temporary Fix for UMAP
    
     # Temporary fix for Procrustes / TSNE
-    if ('TSNE' in input_path) & ('Procrustes' in input_path):
-       print('++ WARNING: Index correction for TSNE.........................................')
-       emb = emb.set_index(['Subject','Window Name'])
-       emb.columns.name = 'TSNE Dimensions'
+    #if ('TSNE' in input_path) & ('Procrustes' in input_path):
+    #   print('++ WARNING: Index correction for TSNE.........................................')
+    #   emb = emb.set_index(['Subject','Window Name'])
+    #   emb.columns.name = 'TSNE Dimensions'
     # Temporary fix for Procrustes / TSNE END 
     n_labels  = emb.index.nlevels
     label_ids = list(emb.index.names)
-    print(' + Number of labels = %d' % n_labels)
-    print(' + Label IDs = %s' % str(label_ids))
-
+    print(' +       Number of labels = %d' % n_labels)
+    print(' +       Label IDs = %s' % str(label_ids))
+    print(' + ================================================================')
     # Remove XXXX windows
     # ===================
-    print('++ INFO: Starting Embedding Shape       = %s' % str(emb.shape))
+    print('++ INFO: Removing Task-Inhomogenous windows prior to SI compuation')
+    print(' +       Original Embedding Shape       = %s' % str(emb.shape))
     if type(emb.index) is pd.MultiIndex:
+       print(' +       Embedding has multi-index --> Group Level Embedding detected.')
        try:
           emb_pure = emb.drop('XXXX',level='Window Name').copy()
+          print(' +       Rows with XXXX in Window Name Index Level successfully removed.')
        except:
           emb_pure = emb.copy()
           print('++ WARNING: Dataframe does not contain XXXX entries. This should only be the case for group-level Procrustes data')
     else:
+       print(' +       Embedding has single-index --> Single-scan Level Embedding detected.')
        try:
           emb_pure = emb.drop('XXXX').copy()
+          print(' +       Rows with XXXX in Window Name Index successfully removed.')
        except:
           emb_pure = emb.copy()
           print('++ WARNING: Dataframe does not contain XXXX entries. This should only be the case for group-level Procrustes data')
     print(' +       Final Embedding DataFrame Size = %s' % str(emb_pure.shape))
-    
+    print(' + ================================================================')
     # Create Empty Dataframe that will hold all computed SI values
     # ============================================================
+    print(' + INFO: Compute Silhouette Index.....')
     df = pd.DataFrame(index=['SI_'+labelID for labelID in label_ids],columns=np.arange(2,m+1))
     df.columns.name = 'm'
     # Compute SI for all possible m values up to the maximum available
@@ -66,13 +71,18 @@ def run(args):
               sel_dims = ['TSNE'+str(i+1).zfill(3) for i in np.arange(m_max)]
           if ('UMAP' in input_path):
               sel_dims = ['UMAP'+str(i+1).zfill(3) for i in np.arange(m_max)]
-          print(' +      [%d/%d]  Dimensions = %s' % (m_max,m,str(sel_dims)))
-          for labelID in label_ids: 
-               si_value  = silhouette_score(emb[sel_dims], emb.reset_index()[labelID], n_jobs=-1)
-               print(' +               SI_%s = %.2f' % (labelID,si_value))
+          print(' +      [%d/%d] Selected Dimensions = %s' % (m_max,m,str(sel_dims)))
+          for labelID in label_ids:
+               input_emb    = emb_pure[sel_dims]
+               input_labels = emb_pure.reset_index()[labelID]
+               print(' +      [%d/%d] Input_Emb.shape=%s | Input_Labels.shape=%s' % (m_max,m,str(input_emb.shape),str(input_labels.shape)))  
+               si_value  = silhouette_score(input_emb, input_labels, n_jobs=-1)
+               print(' +      [%d/%d] SI_%s = %.2f' % (m_max,m,labelID,si_value))
                df.loc['SI_'+labelID,m_max]     = si_value
     print('++ INFO: Evaluation completed...')
+    print('+ ================================================================')
     print(df)
+    print('+ ================================================================')
     
     # Save dataframe with all SI values to disk
     # =========================================
