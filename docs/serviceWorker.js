eb8dd7cd143309389cd1889e8c6ba05b5b6a4932
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2025-02-04 19:31:54 -0500

    now use rawgithub

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'manifold_fmri'
-const appCacheName = 'manifold_fmri-67bb354a3ed04492ba3638ef8a77b6a6';
+const appCacheName = 'manifold_fmri-29127890af13441d84173d73e5cc86d2';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
